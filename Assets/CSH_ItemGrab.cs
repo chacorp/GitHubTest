0aaa6c2a2a24466ad266a0107b4dca83302daaba
chacorp <sihun.cha@gmail.com>
2020-07-22 13:45:38 +0900

    123

--- Assets/CSH_ItemGrab.cs
+++ Assets/CSH_ItemGrab.cs
@@ -7,7 +7,7 @@ public class CSH_ItemGrab : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ㅇㅇㅇㅇㅇ
     }
 
     // Update is called once per frame
