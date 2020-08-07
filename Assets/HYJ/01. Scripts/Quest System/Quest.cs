52b978ce2a33393156e512ad58b108358ae19ff6
TedJunny <65930935+TedJunny@users.noreply.github.com>
2020-08-07 16:29:48 +0900

    퀘스트 시스템 초안

---
+++ Assets/HYJ/01. Scripts/Quest System/Quest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Quest
+{
+    public string title;
+    public string description;
+
+    public bool isActive; // 현재 퀘스트가 진행중인지 판별하는 변수
+
+    public QuestGoal goal;
+
+    public void Complete()
+    {
+        isActive = false;
+        Debug.Log(title + " was completed!!");
+    }
+}
---
+++ Assets/HYJ/01. Scripts/Quest System/QuestManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestManager : MonoBehaviour
+{
+    public List<Quest> quests;
+
+    public PlayerPrefs player;
+
+    public Text titleText;
+    public Text descriptionText;
+    public Text amountText;
+}
